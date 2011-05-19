0dd1e99c088a3cd9d6f1b671c894b35dec21e04b
Rickard Nilsson <rickard@rickardnilsson.net>
2011-05-19 14:46:36 +0200

    jQuery plugin add-stylesheet

---
+++ jQuery/add-stylesheet/jquery.addstylesheet.js
@@ -0,0 +1,20 @@
+﻿$.stylesheets = (function () {
+    var stylesheets,
+        add,
+        clear;
+
+    add = function (cssfile) {
+        $('head').append('<link href="' + cssfile + '" rel="stylesheet" />');
+        return stylesheets;
+    };
+
+    clear = function () {
+        $('head link[rel=stylesheet]').remove();
+        return stylesheets;
+    };
+
+    return stylesheets = {
+        add: add,
+        clear: clear
+    };
+} ());
---
+++ jQuery/add-stylesheet/scripts/jquery.addstylesheet.js
@@ -0,0 +1,20 @@
+﻿$.stylesheets = (function () {
+    var stylesheets,
+        add,
+        clear;
+
+    add = function (cssfile) {
+        $('head').append('<link href="' + cssfile + '" rel="stylesheet" />');
+        return stylesheet;
+    };
+
+    clear = function () {
+        $('head link[rel=stylesheet]').remove();
+        return stylesheet;
+    };
+
+    return stylesheets = {
+        add: add,
+        clear: clear
+    };
+} ());
